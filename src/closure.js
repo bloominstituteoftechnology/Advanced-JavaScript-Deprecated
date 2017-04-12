b5b5ebb85200c92df1eb770142594828d52522bf
sunjieming <ben@lambdaschool.com>
2017-04-12 09:38:28 -0600

    Add solutions

--- src/closure.js
+++ src/closure.js
@@ -3,6 +3,8 @@
 /* eslint-disable no-unused-vars */
 
 const counter = () => {
+  let count = 0;
+  return () => (++count);
   // Return a function that when invoked increments and returns a counter variable.
   // Example: const newCounter = counter();
   // newCounter(); // 1
@@ -10,17 +12,34 @@ const counter = () => {
 };
 
 const counterFactory = () => {
+  let count = 0;
+  return {
+    increment: () => (++count),
+    decrement: () => (--count),
+  };
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
 
 const limitFunctionCallCount = (cb, n) => {
+  let callCount = 0;
+  return (...args) => {
+    if (callCount === n) return null;
+    callCount++;
+    return cb(...args);
+  };
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
 };
 
 const cacheFunction = (cb) => {
+  const cache = {};
+  return (input) => {
+    if (Object.prototype.hasOwnProperty.call(cache, input)) return cache[input];
+    cache[input] = cb(input);
+    return cache[input];
+  };
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
