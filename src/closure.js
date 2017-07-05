b644c697d53c844c94cd8093f00a763e45c1e723
LOLH <lincolnlaw@mac.com>
2017-07-05 12:52:13 -0700

    finished closure.js

--- src/closure.js
+++ src/closure.js
@@ -5,17 +5,38 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+
+  let count = 0;
+
+  return () => ++count;
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
+  let count = 0;
+
+  return {
+    increment: () => ++count,
+    decrement: () => --count,
+  };
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+
+  let invoked = 0;
+
+  return (...args) => {
+    if (invoked < n) {
+      invoked++;
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 const cacheFunction = (cb) => {
@@ -25,6 +46,47 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+
+  const usedArgs = [];
+
+  const compareCached = (x, y) => {
+    if (x.length !== y.length) {
+      return false;
+    }
+
+    return x.some(e => y.indexOf(e) !== -1);
+  };
+
+  const cached = (...args) => {
+    for (let i = 0; i < usedArgs.length; i++) {
+      const usedArg = usedArgs[i];
+      if (compareCached(args, usedArg.args)) {
+        return usedArg.theResult;
+      }
+    }
+
+    return null;
+  };
+
+  const cacheResult = (result, ...args) => {
+    usedArgs.push({
+      args: args.sort(),
+      theResult: result,
+    });
+  };
+
+  return (...args) => {
+    const result = cached(...args);
+
+    if (result !== null) {
+      return result;
+    }
+
+    const newResult = cb(...args);
+    cacheResult(newResult, ...args);
+
+    return newResult;
+  };
 };
 
 /* eslint-enable no-unused-vars */
