f7a3c057afec7d4c8966dbe7d4acf2dbedcd5b3a
sfmajors373 <sfmajors373@gmail.com>
2017-07-06 22:51:50 +0000

    unfucked

---
+++ src/objects.js
@@ -0,0 +1,80 @@
+// Complete the following underscore functions.
+// Reference http://underscorejs.org/ for examples.
+
+const keys = (obj) => {
+  // Retrieve all the names of the object's properties.
+  // Return the keys as strings in an array.
+  // Based on http://underscorejs.org/#keys
+  const arr = [];
+  for (const key in obj) {
+    arr.push(key);
+  }
+  return arr;
+};
+
+const values = (obj) => {
+  // Return all of the values of the object's own properties.
+  // Ignore functions
+  // http://underscorejs.org/#values
+  const arr = [];
+  for (const key in obj) {
+    arr.push(obj[key]);
+  }
+  return arr;
+};
+
+const mapObject = (obj, cb) => {
+  // Like map for arrays, but for objects. Transform the value of each property in turn.
+  // http://underscorejs.org/#mapObject
+  const newObj = {};
+  for (const key in obj) {
+    newObj[key] = (cb(obj[key]));
+  }
+  return newObj;
+};
+
+const pairs = (obj) => {
+  // Convert an object into a list of [key, value] pairs.
+  // http://underscorejs.org/#pairs
+  const arr = [];
+  for (const key in obj) {
+    arr.push([key, obj[key]]);
+  }
+  return arr;
+};
+
+const invert = (obj) => {
+  // Returns a copy of the object where the keys have become the values and the values the keys.
+  // Assume that all of the object's values will be unique and string serializable.
+  // http://underscorejs.org/#invert
+  const newObj = {};
+  for (const key in obj) {
+    newObj[obj[key]] = key;
+  }
+  return newObj;
+};
+
+const defaults = (obj, defaultProps) => {
+  // Fill in undefined properties that match properties on the `defaultProps` parameter object.
+  // Return `obj`.
+  // http://underscorejs.org/#defaults
+  for (const key in defaultProps) {
+    if (key in obj){
+      continue;
+    } else {
+      obj[key] = defaultProps[key];
+    }
+  }
+  return obj;
+};
+
+/* eslint-enable no-unused-vars */
+
+module.exports = {
+  keys,
+  values,
+  mapObject,
+  pairs,
+  invert,
+  defaults
+};
