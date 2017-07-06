790b33463061fc35812f3a8864c54a5daa18d58d
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-07-06 07:12:37 -0400

    some this tests passing

--- src/this.js
+++ src/this.js
@@ -5,6 +5,11 @@
 class User {
   constructor(options) {
     // set a username and password property on the user object that is created
+    this.username = options.username;
+    this.password = options.password;
+  }
+  checkPassword (str) {
+    return str === this.password;
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
@@ -15,11 +20,13 @@ const me = new User({ username: 'LambdaSchool', password: 'correcthorsebatteryst
 const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
 
 const checkPassword = function comparePasswords(passwordToCompare) {
+  return this.password === passwordToCompare;
   // recreate the `checkPassword` method that you made on the `User` class
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
 };
+console.log(result, checkPassword(me.password));
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
