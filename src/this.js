579d42eb159ca1cceda264d48c051acd5cbcffa0
mixelpixel <mixelpix@mac.com>
2017-07-06 18:41:12 -0400

    npm weirdness?

--- src/this.js
+++ src/this.js
@@ -1,38 +1,38 @@
-// Follow the instructions and fill in the blank sections.
-// There are no tests for this file.
-// To verify your code works you can run this file using `node this.js` while in the `/src` folder
-
-class User {
-  constructor(options) {
-    // set a username and password property on the user object that is created
-    this.username = options.username,
-    this.password = options.password
-  }
-  // create a method on the User class called `checkPassword`
-  // this method should take in a string and compare it to the object's password property
-  // return `true` if they match, otherwise return `false`
-  checkPassword(compareString) {
-    return compareString === this.password;
-  }
-}
-
-const me = new User({ username: 'LambdaSchool', password: 'correcthorsebatterystaple' });
-const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
-
-console.log(result);
-
-const checkPassword = function comparePasswords(passwordToCompare) {
-  // recreate the `checkPassword` method that you made on the `User` class
-  // use `this` to access the object's `password` property.
-  // do not modify this function's parameters
-  // note that we use the `function` keyword and not `=>`
-};
-
-// invoke `checkPassword` on `me` by explicitly setting the `this` context
-// use .call, .apply, and .bind
-
-// .call
-
-// .apply
-
-// .bind
+// // Follow the instructions and fill in the blank sections.
+// // There are no tests for this file.
+// // To verify your code works you can run this file using `node this.js` while in the `/src` folder
+//
+// class User {
+//   constructor(options) {
+//     // set a username and password property on the user object that is created
+//     this.username = options.username,
+//     this.password = options.password
+//   }
+//   // create a method on the User class called `checkPassword`
+//   // this method should take in a string and compare it to the object's password property
+//   // return `true` if they match, otherwise return `false`
+//   checkPassword(compareString) {
+//     return compareString === this.password;
+//   }
+// }
+//
+// const me = new User({ username: 'LambdaSchool', password: 'correcthorsebatterystaple' });
+// const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
+//
+// // console.log(result);
+//
+// const checkPassword = function comparePasswords(passwordToCompare) {
+//   // recreate the `checkPassword` method that you made on the `User` class
+//   // use `this` to access the object's `password` property.
+//   // do not modify this function's parameters
+//   // note that we use the `function` keyword and not `=>`
+// };
+//
+// // invoke `checkPassword` on `me` by explicitly setting the `this` context
+// // use .call, .apply, and .bind
+//
+// // .call
+//
+// // .apply
+//
+// // .bind
