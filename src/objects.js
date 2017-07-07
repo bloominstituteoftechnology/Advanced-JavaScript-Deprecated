acc7a48c0a0d120b2de0de7fba5809e5434c9f54
sfmajors373 <sfmajors373@gmail.com>
2017-07-07 02:46:29 +0000

    started fixing linting issues

--- src/objects.js
+++ src/objects.js
@@ -26,45 +26,38 @@ const values = (obj) => {
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
-  const newObj = {};
-  for (const key in obj) {
-    newObj[key] = (cb(obj[key]));
-  }
-  return newObj;
+  Object.keys(obj).forEach((key) => {
+    obj[key] = cb(obj[key]);
+  });
+  return obj;
 };
 
-const pairs = (obj) => {
-  // Convert an object into a list of [key, value] pairs.
-  // http://underscorejs.org/#pairs
-  const arr = [];
-  for (const key in obj) {
-    arr.push([key, obj[key]]);
-  }
-  return arr;
-};
+const pairs = obj => (Object.keys(obj).map(key => ([key, obj[key]])));
+// Convert an object into a list of [key, value] pairs.
+// http://underscorejs.org/#pairs
 
 const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
-  const newObj = {};
-  for (const key in obj) {
-    newObj[obj[key]] = key;
-  }
-  return newObj;
+  Object.keys(obj).forEach((key) => {
+    const tempKey = obj[key];
+    obj[tempKey] = key;
+    delete obj[key];
+  });
+  return obj;
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
-  for (const key in defaultProps) {
-    if (key in obj){
-      continue;
-    } else {
-      obj[key] = defaultProps[key];
+  Object.keys(defaultProps).forEach((key) => {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return;
     }
-  }
+    obj[key] = defaultProps[key];
+  });
   return obj;
 };
 
