e0614755d5404e3f0d7d9949f39020623e77ec6d
mixelpixel <mixelpix@mac.com>
2017-07-09 16:08:39 -0400

    all array tests pass

--- src/arrays.js
+++ src/arrays.js
@@ -8,6 +8,7 @@ const each = (elements, cb) => {
   // Q: I AM CONFUSED, WHY DOES CB TAKE 2 PARAMETERS???
   // A: somewhat arbitrary, discernable from the test suite
   //    just emulating forEach and underscore's each
+
   // version 1
   for (let i = 0; i < elements.length; i++) {
     cb(elements[i], i);
@@ -18,7 +19,7 @@ const each = (elements, cb) => {
   //   cb(value, index);
   // });
 
-  // version 3 - NOPE - OH, HA! I'd have to install underscore for this to work...
+  // version 3 - jsfiddle has underscore library
   // _.each(elements, (value, index) => {
   //   cb(value, index); // <--- ??? Not sure that's it
   // });
@@ -27,8 +28,9 @@ const each = (elements, cb) => {
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+
   // version 1 √
-  // // const newArray = new Array();
+  // // const newArray = new Array(); <--- no bueno syntax
   // const newArray = [];
   // for (let i = 0; i < elements.length; i++) {
   //   newArray.push(cb(elements[i]));
@@ -55,6 +57,7 @@ const map = (elements, cb) => {
 //   Combine all elements into a single value going from left to right.
 //   Elements will be passed one by one into `cb`.
 //   `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+//
 //   version 1 √
 //   for (let i = 0; i < elements.length; i++) {
 //     memo = cb(memo, elements[i]);
@@ -73,6 +76,7 @@ const reduce = (elements, cb, memo) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+
   // version 1 √
   // elements NOT passed into cb
   // let sum;
@@ -89,22 +93,50 @@ const reduce = (elements, cb, memo) => {
 
   // version 2 √
   // elements passed into cb
-  let sum;
-  if (typeof (elements[0]) === 'number') {
-    sum = 0;
-    if (memo) sum = memo;
-  } else if (typeof (elements[0]) === 'string') {
-    sum = '';
-    if (memo) sum = memo;
-  }
-  for (let i = 0; i < elements.length; i++) {
-    sum = cb(sum, elements[i]);
-  } return sum;
+  // let sum;
+  // if (typeof (elements[0]) === 'number') {
+  //   sum = 0;
+  //   if (memo) sum = memo;
+  // } else if (typeof (elements[0]) === 'string') {
+  //   sum = '';
+  //   if (memo) sum = memo;
+  // }
+  // for (let i = 0; i < elements.length; i++) {
+  //   sum = cb(sum, elements[i]);
+  // } return sum;
 
   // version 3 - NOPE
   // solution using reduce()
   // const total = elements.reduce(cb, memo);
   // return total;
+
+  // Karthik's version A: passing the 2nd test only, "✓ should accept a memo argument"
+  // each(elements, (elem) => {
+  //   memo = cb(memo, elem); // <-- returns a new memo which is used for the next callback
+  // });
+  // return memo;
+
+  // Karthik's version B: passes all tests
+  // if (memo === undefined) {
+  //   memo = elements.shift(); // <--- removes first element from array and returns it to memo variable
+  // }
+  // each(elements, (elem) => {
+  //   memo = cb(memo, elem);
+  // });
+  // return memo;
+
+  // Karthik's version C: a better way
+  let i = 0;
+  if (memo === undefined) {
+    memo = elements[0];
+    i = 1; // <--- starting at second item when no memo is passed in
+  }
+
+  for (; i < elements.length; i++) {
+    memo = cb(memo, elements[i]);
+  }
+  return memo;
+
 
   // Ryan's solution √
   // no string/number check, nice!
@@ -125,12 +157,13 @@ const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+
   // version 1 √
   for (let i = 0; i < elements.length; i++) {
     if (cb(elements[i]) === true) {
       return elements[i];
     }
-    // Q: why does uncommenting this out make it fail?????
+    // Q: why does uncommenting this return out make it fail?????
     // return undefined;
   }
 };
@@ -138,6 +171,7 @@ const find = (elements, cb) => {
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+
   // version 1 √
   const trueArray = []; // ----> WHY ISN'T THIS let???
   for (let i = 0; i < elements.length; i++) {
@@ -151,12 +185,12 @@ const filter = (elements, cb) => {
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+
   // version 1 √
   let flat = [];
   for (let i = 0; i < elements.length; i++) {
     // if (elements[i] instanceof Array) { // <---- Also works
     if (Array.isArray(elements[i])) {
-      // flat = flat.push(flatten(elements[i])); // <--- nope
       flat = flat.concat(flatten(elements[i]));
     } else {
       flat.push(elements[i]);
