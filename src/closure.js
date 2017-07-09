004662fa17232625a4d5897bebc922a049c823cf
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-07-09 04:55:05 -0400

    all closure tests passing

--- src/closure.js
+++ src/closure.js
@@ -1,44 +1,37 @@
 // Complete the following functions.
 
 const counter = () => {
-  let count = 0;
-  const i = () => ++count;
-  return i;
+  let ct = 0;
+  return () => ++ct;
 };
 
 const counterFactory = () => {
-  let count = 0;
-  const obj = {
+  let ct = 0;
+  return {
     increment() {
-      return ++count;
+      return ++ct;
     },
     decrement() {
-      return --count;
+      return --ct;
     }
   };
-  return obj;
 };
 
 const limitFunctionCallCount = (cb, n) => {
-  const limit = n;
-  let count = 0;
-  const i = (...args) => {
-    if (count < limit) {
-      count++;
+  let l = n;
+  return (...args) => {
+    if (l > 0) {
+      l--;
       return cb(...args);
-    }
-    return null;
+    } return null;
   };
-  return i;
 };
 
 const cacheFunction = (cb) => {
-  // Should return a funciton that invokes `cb`.
-  // A cache (object) should be kept in closure scope.
-  // The cache should keep track of all arguments have been used to invoke this function.
-  // If the returned function is invoked with arguments that it has already seen
-  // then it should return the cached result and not invoke `cb` again.
-  // `cb` should only ever be invoked once for a given set of arguments.
+  const c = {};
+  return (n) => {
+    return n in c ? c[n] : c[n] = cb(n);
+  };
 };
 
 /* eslint-enable no-unused-vars */
