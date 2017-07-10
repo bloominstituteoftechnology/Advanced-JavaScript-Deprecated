7584ed78c45917ffe6129d14cb45c26a03b44b2c
mixelpixel <mixelpix@mac.com>
2017-07-10 14:43:40 -0400

    commented out w.i.p. for matchingLeaves

--- src/recursion.js
+++ src/recursion.js
@@ -43,38 +43,38 @@ const checkMatchingLeaves = (obj) => {
 
   // version 1 - solve for first test ONLY √
   // compare one object value against (itself and) all other values
-  const x = Object.keys(obj);
-  // const x = Object.getOwnPropertyNames(obj); // <--- same as Object.keys()
-  const y = x[0];
-
-  for (let i = 0; i < x.length; i++) {
-    if (obj[y] !== obj[x[i]]) return false;
-  }
-  return true;
-
-  // version 3 - working towards depth check, iterative
-  const tree1 = { x: 1, y: 1, z: 1, xa: { xx: 1, xy: 1, xz: 1, zz: { a: { b: { z: 1, }, }, }, }, };
-
-  const k = Object.keys(tree1);
-
-  console.log(k);
-
-  const anArray = []
-
-  for (y in tree1) {
-    anArray.push([]);
-  };
-
-  console.log(anArray);
-
-  for (b in tree1) {
-    console.log(`${b} ${typeof b} ${tree1[b]} ${typeof tree1[b]}`);
-    if (typeof tree1[b] === 'object') {
-      for (c in tree1[b]) {
-      console.log(`${c} ${typeof c} ${tree1[b][c]} ${typeof tree1[b][c]}`);
-      }
-    }
-  };
+  // const x = Object.keys(obj);
+  // // const x = Object.getOwnPropertyNames(obj); // <--- same as Object.keys()
+  // const y = x[0];
+  //
+  // for (let i = 0; i < x.length; i++) {
+  //   if (obj[y] !== obj[x[i]]) return false;
+  // }
+  // return true;
+  //
+  // // version 3 - working towards depth check, iterative
+  // const tree1 = { x: 1, y: 1, z: 1, xa: { xx: 1, xy: 1, xz: 1, zz: { a: { b: { z: 1, }, }, }, }, };
+  //
+  // const k = Object.keys(tree1);
+  //
+  // console.log(k);
+  //
+  // const anArray = []
+  //
+  // for (y in tree1) {
+  //   anArray.push([]);
+  // };
+  //
+  // console.log(anArray);
+  //
+  // for (b in tree1) {
+  //   console.log(`${b} ${typeof b} ${tree1[b]} ${typeof tree1[b]}`);
+  //   if (typeof tree1[b] === 'object') {
+  //     for (c in tree1[b]) {
+  //     console.log(`${c} ${typeof c} ${tree1[b][c]} ${typeof tree1[b][c]}`);
+  //     }
+  //   }
+  // };
 
 
   // version 2 - NOPE
