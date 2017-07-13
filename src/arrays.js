952038a4eeaf064be38484d00b5468e5ae5872a6
Nana Owusu Nyamekye <nanaowusu1234@gmail.com>
2017-07-13 03:15:27 +0000

    Halfway Through 'arrays.js'

--- src/arrays.js
+++ src/arrays.js
@@ -6,23 +6,56 @@ const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   // based off http://underscorejs.org/#each
+
+  elements.forEach((item, index) => {
+    cb(item, index);
+  });
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+
+  const myArray = elements.map((item, index) => {
+    return cb(item, index);
+  });
+
+  return myArray;
 };
 
 const reduce = (elements, cb, memo) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+
+  // let sum;
+  // if (memo === undefined) sum = elements[0];
+  // memo.reduce((sum, elements) => {
+  //   return cb(elements);
+  // });
+
+  // if (memo === undefined) {
+  //   memo = elements[0];
+  // }
+  // for (let i = 0; i < memo.length; i++) {
+  //   memo.push.cb(elements);
+  // }
+  if (!memo) {
+    memo = elements[0];
+  }
+  for (let i = 0; i < elements.length; i++) {
+    memo = cb(memo, elements[i]);
+  }
+  return memo;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+
+  for (let i = 0; i < elements.length; i++) if (cb(elements[i]) === true) return elements[i];
+  return 'undefined';
 };
 
 const filter = (elements, cb) => {
