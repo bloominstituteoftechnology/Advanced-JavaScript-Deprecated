3cf152d128ce36d49c4c53e0b6bbcfae56cd3e2e
Pratik Sampat <pratiks0809@gmail.com>
2017-07-15 18:32:19 -0500

    finished up to closures.js

--- src/closure.js
+++ src/closure.js
@@ -16,11 +16,30 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let counterItem = 0;
+  return {
+    increment: () => {
+      counterItem++;
+      return counterItem;
+    },
+    decrement: () => {
+      counterItem--;
+      return counterItem;
+    }
+  };
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let counterNumber3 = 1;
+  let invocationLimit = n;
+  return (...args) => {
+    if (counterNumber3 <= invocationLimit) {
+      counterNumber3++;
+      return cb(...args);
+    } return null;
+  }
 };
 
 const cacheFunction = (cb) => {
@@ -30,6 +49,18 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  let cache = {};
+  cache = {
+    values: [10, 2, 5, true]
+  }
+  return (...args) => {
+      if(cache.hasOwnProperty(args)) {
+        return cache[args];
+      }
+      cache[args] = cb(args);
+      return cache[args];
+  };
+
 };
 
 /* eslint-enable no-unused-vars */
