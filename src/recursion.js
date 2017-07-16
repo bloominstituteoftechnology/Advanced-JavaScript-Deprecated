18df7ffb59c3d8c8bba77e2402f0cc2aa53d648e
mixelpixel <mixelpix@mac.com>
2017-07-16 14:06:05 -0400

    matchingLeaves

--- src/recursion.js
+++ src/recursion.js
@@ -39,30 +39,64 @@ const nFactorial = (n) => {
 };
 
 const checkMatchingLeaves = (obj) => {
-  // return true if every property on `obj` is the same
+  //                                             { key: value, key:  vv value vv }
+  // INPUT: will either be a number or an object {   x:   1  ,   y: {key: value} }
+  //                                             typeof obj[y] >>> 'object'
+  // FUNCTION:
+  // leafValue
+  // recursive function()
+    // iterate over the keys on obj
+      // find first non object value         vvv    recursion    vvv
+      // if object[key] is an object, then checkMatchingLeaves(obj[key])
+      // if leafValue is undefined set it to obj[key]
+      // check if leafValue is equal to obj[key] - otherwise return false
+      // DEPTH FIRST SEARCH with recursion
+  // call recursive function()
+  // OUTPUT: return true if every property on `obj` is the same
   // otherwise return false
 
   // version 1 √
-
-  // // Ben's solution √
-  // let val;
-  // let allMatch = true;
-  // const checkLeaves = (object) => {
-  //   Object.keys(object).forEach((key) => {
-  //     if (val === undefined && typeof key !== 'object') {
-  //       val = object[key];
-  //       return undefined;
-  //     }
-  //     if (typeof object[key] === 'object') return checkLeaves(object[key]);
-  //     if (object[key] !== val) {
-  //       allMatch = false;
-  //       return undefined;
-  //     }
-  //     return undefined;
-  //   });
+  // example nested object: const tree = { x: 1, y: 1, z: 1, xa: { xx: 1, xy: 1, xz: 1, zz: { a: { b: { z: 1, }, }, }, y: 2 }, };
+  // const tree1 = {
+  //   x: 1,
+  //   y: 1,
+  //   z: 1,
+  //   xa: {
+  //     xx: 1,
+  //     xy: 1,
+  //     xz: 1,
+  //     zz: {
+  //       a: {
+  //         b: {
+  //           z: 1,
+  //         },
+  //       },
+  //     },
+  //     y: 2
+  //   },
   // };
-  // checkLeaves(obj);
-  // return allMatch;
+
+  // version 1 - Ben's solution √
+  let leafValue;
+  let leavesMatch = true;
+  const checkLeaf = (object) => {
+    Object.keys(object).forEach((key) => {
+      // if (leafValue === undefined && typeof key !== 'object') {
+      if (leafValue === undefined && key instanceof Object !== true) {
+        leafValue = object[key];
+        return undefined;
+      }
+      // if (typeof object[key] === 'object') return checkLeaf(object[key]); // <--- recursive
+      if (object[key] instanceof Object === true) return checkLeaf(object[key]); // <--- recursive
+      if (object[key] !== leafValue) {
+        leavesMatch = false;
+        return undefined;
+      }
+      return undefined;
+    });
+  };
+  checkLeaf(obj);
+  return leavesMatch;
 };
 
 
