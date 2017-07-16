10e7123682fa933c81fc5b15d578bddf0cc1599e
Melissa <melissaeaybar@gmail.com>
2017-07-16 00:00:18 +0000

    advanced.js

--- src/arrays.js
+++ src/arrays.js
@@ -1,3 +1,4 @@
+/*
 // Complete the following functions.
 // These functions only need to work with arrays.
 
@@ -11,32 +12,80 @@ const each = (elements, cb) => {
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+   const arr = [];
+ 
+   for (let i = 0; i < elements.length; i++) {
+     arr[i] = cb(elements[i]);
+   }
+ 
+   return arr;
 };
 
 const reduce = (elements, cb, memo) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+  let reduced = (memo === undefined) ? elements.shift() : memo;
+ 
+   for (let i = 0; i < elements.length; i++) {
+     reduced += elements[i];
+   }
+ 
+   return reduced;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+
+  for (let i = 0; i < elements.length; i++) {
+     if (cb(elements[i])) {
+       return elements[i];
+     }
+   }
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const filtered = [];
+
+   for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+       filtered.push(elements[i]);
+     }
+   }
+
+   return filtered;
+
 };
 
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+     onst flattener = (els, arr) => {
+       if (abc.length > 0) {
+       const el = abc.shift();
+       if (el instanceof Array) {
+         flattener(x, arr);
+       } else {
+         arr.push(x);
+       }
+ 
+       flattener(abc, arr);
+     }
+ 
+     return arr;
+   };
+ 
+   return flattener(elements, []);
+  };
+  
 };
 
-/* eslint-enable no-unused-vars, max-len */
 
+/*
 module.exports = {
   each,
   map,
@@ -45,3 +94,5 @@ module.exports = {
   filter,
   flatten
 };
+
+*/
--- src/closure.js
+++ src/closure.js
@@ -1,3 +1,4 @@
+
 // Complete the following functions.
 
 const counter = () => {
@@ -5,17 +6,30 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count = 0;
+  return ()=> ++count;
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  return {
+     increment: () => ++count,
+     decrement: () => --count,
+
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let stuff=0;
+  return (...args) => {
+     if (stuff < n) {
+       stuff++;
+       return cb(...args);
+     }
+     return null;
 };
 
 const cacheFunction = (cb) => {
@@ -25,9 +39,9 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
-};
 
-/* eslint-enable no-unused-vars */
+
+
 
 module.exports = {
   counter,
@@ -35,3 +49,4 @@ module.exports = {
   cacheFunction,
   limitFunctionCallCount
 };
+
