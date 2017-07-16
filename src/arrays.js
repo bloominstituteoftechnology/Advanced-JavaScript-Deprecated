4d5290bd6e63dad4485bf78e01e70711fa2546b9
Vance Villanueva <vancervillanueva@gmail.com>
2017-07-15 21:05:46 -0700

    Finished Arrays.js

--- src/arrays.js
+++ src/arrays.js
@@ -21,30 +21,55 @@ const map = (elements, cb) => {
   return newArray;
 };
 
-const reduce = (elements, cb, memo) => {
+const reduce = (elements, cb, memo = elements.shift()) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+  for (let i = 0; i < elements.length; i++) {
+    memo = cb(memo, elements[i]);
+  }
+  return memo;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
-  each(elements, (value, index) => {
-    return cb
-  });
-  return 'undefined';
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      return elements[i];
+    }
+  }
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      newArr.push(elements[i]);
+    }
+  }
+  return newArr;
 };
 
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (Array.isArray(elements[i])) {
+      const nestedArray = flatten(elements[i]);
+      for (let j = 0; j < nestedArray.length; j++) {
+        newArray.push(nestedArray[j]);
+      }
+    } else {
+      newArray.push(elements[i]);
+    }
+  }
+  return newArray;
 };
 
 /* eslint-enable no-unused-vars, max-len */
