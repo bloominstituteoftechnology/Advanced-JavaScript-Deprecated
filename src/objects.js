775e081b726a07c69fd1a309a2ba656f7c6e6f74
Vance Villanueva <vancervillanueva@gmail.com>
2017-07-17 02:11:34 -0700

    Touched object, class, and this

--- src/objects.js
+++ src/objects.js
@@ -5,34 +5,51 @@ const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  return Object.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  return Object.values(obj);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  const map = new Map(cb(Object.entries(obj)));
+  return map;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  return Object.entries(obj);
 };
 
 const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  const newData = Object.keys(obj).reduce((obj1, key) => Object.assign({}, obj1, { [obj[key]]: key }), {});
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  const nObj = {};
+  const prop = Object.keys(obj);
+  const value = Object.values(obj);
+  for (let i = 0; i < prop.length; i++) {
+    if (prop[i] === defaultProps) {
+      nObj.i = undefined;
+    } else {
+      nObj.i = value[i];
+    }
+  }
+  return nObj;
 };
 
 /* eslint-enable no-unused-vars */
