ea87083b0c48d4c22ca65a61ed6799fbd2ab2375
bobby brady <bloopsocket@gmail.com>
2017-07-18 19:45:20 +0000

    updated: finished arrays

--- src/arrays.js
+++ src/arrays.js
@@ -20,27 +20,54 @@ const map = (elements, cb) => {
   return newArray;
 };
 
-const reduce = (elements, cb, memo) => {
+const reduce = (elements, cb, memo = elements.shift()) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+  elements.forEach((i) => {
+    memo = cb(memo, i);
+  });
+  return memo;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      return elements[i];
+    }
+  }
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const truth = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i]) === true) {
+      truth.push(elements[i]);
+    }
+  }
+  return truth;
 };
 
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
-  return elements.reduce((acc, cur) => acc.concat(cur), []);
+  const newArray = [];
+  elements.forEach((i) => {
+    if (Array.isArray(i) === true) {
+      const nestedArray = flatten(i);
+      nestedArray.forEach((j) => {
+        newArray.push(j);
+      });
+    } else {
+      newArray.push(i);
+    }
+  });
+  return newArray;
 };
 
 /* eslint-enable no-unused-vars, max-len */
