3059b7c7cd59465af36e6b43e302252733ef9980
bobby brady <bloopsocket@gmail.com>
2017-07-21 14:49:52 +0000

    apply stash for lost work

--- src/recursion.js
+++ src/recursion.js
@@ -3,16 +3,31 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 1) {
+    return 1;
+  }
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 0) {
+    return 1;
+  }
+  return n * nFactorial(n - 1);
 };
 
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  obj.forEach((prop) => {
+    if (Object(hasOwnProperty(prop))) {
+      return true;
+      // stuck and lost on how to solve this
+    }
+  });
+  return false;
 };
 
 /* eslint-enable no-unused-vars */
