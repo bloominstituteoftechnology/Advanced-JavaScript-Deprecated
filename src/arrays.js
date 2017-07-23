4ce186711fc28a0ce8cc5180124035a7bfa10779
Alex <anova@protonmail.ch>
2017-07-23 00:17:42 -0400

    Flatten problem left
    A. Novak completed all tests of arrays except Flatten
    7/22

--- src/arrays.js
+++ src/arrays.js
@@ -6,28 +6,60 @@ const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   // based off http://underscorejs.org/#each
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArray[i] = cb(elements[i]);
+  }
+  return newArray;
 };
 
 const reduce = (elements, cb, memo) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+  if (memo === undefined) {
+    memo = elements[0];
+    for (let i = 1; i < elements.length; i++) {
+      memo = cb(memo, elements[i]);
+    }
+  } else {
+    for (let i = 0; i < elements.length; i++) {
+      memo = cb(memo, elements[i]);
+    }
+  }
+  return memo;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      return elements[i];
+    }
+  }
+  return 'undefined';
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const filterArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      filterArray.push(elements[i]);
+    }
+  }
+  return filterArray;
 };
 
 const flatten = (elements) => {
