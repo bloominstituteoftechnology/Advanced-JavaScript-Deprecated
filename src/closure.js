6bb17f71a90a7283065393eb4a2ce0b7d50d1b91
PotatoSalads <anova@protonmail.ch>
2017-08-01 05:55:18 -0400

    Completed closure 8/1

--- src/closure.js
+++ src/closure.js
@@ -5,17 +5,31 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let counts = 0;
+  return function newCounter() { return counts += 1; };
 };
 
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let num = 0;
+  return {
+    increment: () => num += 1,
+    decrement: () => num -= 1
+  };
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let num = 0;
+  return (...args) => {
+    if (num < n) {
+      num++;
+      return cb(...args);
+    } return null;
+  };
 };
 
 const cacheFunction = (cb) => {
@@ -25,6 +39,15 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  // made sure to get rid of warning for not naming the function
+  const cache = {};
+  return function cacheReturn(property) {
+    if (Object.prototype.hasOwnProperty.call(cache, property)) {
+      return cache[property];
+    }
+    cache[property] = cb(property);
+    return cache[property];
+  };
 };
 
 /* eslint-enable no-unused-vars */
