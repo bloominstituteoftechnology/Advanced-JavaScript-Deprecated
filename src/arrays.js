b46683558d6250c1c9c33e672b2ac8da5474ff4d
codealtgeek <codealtgeek@gmail.com>
2017-08-09 14:07:44 -0400

    Array Completed

--- src/arrays.js
+++ src/arrays.js
@@ -9,35 +9,64 @@ const each = (elements, cb) => {
   // This only needs to work with arrays.
   // based off http://underscorejs.org/#each
   for (let i = 0; i < elements.length; i++) {
-    cb(elements[i]);
+    cb(elements[i], i);
   }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const array = [];
+  for (let i = 0; i < elements.length; i++) {
+    array.push(cb(elements[i], i));
+  }
+  return array;
 };
 
 const reduce = (elements, cb, memo = elements.shift()) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+  let sum = memo;
+  for (let i = 0; i < elements.length; i++) {
+    sum = cb(sum, elements[i]);
+  }
+  return sum;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  let value;
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      value = elements[i];
+      break;
+    }
+    value = undefined;
+  }
+  return value;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const passedTest = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) passedTest.push(elements[i]);
+  }
+  return passedTest;
 };
 
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  const flattenedArray = reduce(elements, (memo, item) => {
+    if (Array.isArray(item)) return memo.concat(flatten(item));
+    return memo.concat(item);
+  }, []);
+  return flattenedArray;
 };
 
 /* eslint-enable no-unused-vars, max-len */
