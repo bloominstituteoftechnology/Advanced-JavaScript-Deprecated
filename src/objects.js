3b0d3b308907cad04d3b5f2d1e7341801ab7a9c5
Camucamu <drewmueller4@gmail.com>
2017-08-10 13:42:51 -0700

    problem with values

--- src/objects.js
+++ src/objects.js
@@ -1,27 +1,29 @@
 // Complete the following underscore functions.
 // Reference http://underscorejs.org/ for examples.
 
-const keys = (obj) => {
+const keys = obj => Object.keys(obj);
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
-};
 
-const values = (obj) => {
+
+const values = obj => Object.values(obj);
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
-};
+
 
 const mapObject = (obj, cb) => {
+  Object.keys(obj).forEach(key => (obj[key]) = cb(obj[key]));
+  return obj;
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
 };
 
-const pairs = (obj) => {
+const pairs = obj => Object.entries(obj);
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
-};
+
 
 const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
