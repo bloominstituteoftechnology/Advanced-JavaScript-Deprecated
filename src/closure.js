f1aeed0b66ed4f109ee6fd4a89fb19d5b31dba98
Dylan <dylan@Dylans-MacBook.local>
2017-08-10 14:16:46 -0400

    completed closure

--- src/closure.js
+++ src/closure.js
@@ -54,6 +54,14 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = [];
+  return (...args) => {
+    if (!cache.includes(...args)) {
+      cache.push(...args);
+      return cb(...args);
+    }
+    return cache;
+  };
 };
 
 /* eslint-enable no-unused-vars */
