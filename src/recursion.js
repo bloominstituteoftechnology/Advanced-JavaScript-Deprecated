518837da0e81f82a36bdad6fa3d320ba80b591e7
Dylan <dylan@Dylans-MacBook.local>
2017-08-10 18:21:07 -0400

    Completed AJ

--- src/recursion.js
+++ src/recursion.js
@@ -1,6 +1,12 @@
 // Complete the following functions.
 
 const nFibonacci = (n) => {
+  if (n === 0) {
+    return 1;
+  } else if (n < 0) {
+    return 0;
+  }
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
 };
@@ -8,11 +14,27 @@ const nFibonacci = (n) => {
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 1) {
+    return 1;
+  }
+  return n * nFactorial(n - 1);
 };
+
 
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  const flat = [];
+  const recursion = (...args) => {
+    Object.values(...args).forEach((value) => {
+      if (typeof value === 'object') {
+        return recursion(value);
+      }
+      flat.push(value);
+    });
+    return flat.every(p => p === flat[0]);
+  };
+  return recursion(obj);
 };
 
 /* eslint-enable no-unused-vars */
