801a572dc57504f6af9c66fa2ff6632786a5bf1b
Frank Faustino <nawzyah@gmail.com>
2017-08-11 01:25:18 -0700

    Completed class.js

--- src/class.js
+++ src/class.js
@@ -5,10 +5,18 @@
 // Add a method called `comparePasswords`.  `comparePasswords` should have a parameter
 // for a potential password that will be compared to the `password` property.
 // Return true if the potential password matches the `password` property.  Otherwise return false.
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+  }
+  comparePasswords(x) {
+    return this.password === x;
+  }
+}
 
-
-/* eslint-disable no-undef */ // Remove this comment once you write your classes.
-
+const me = new User({ email: 'email@email.com', password: 'p4$sw0rd' });
+console.log(me.comparePasswords('passwerd')); // false
 
 // Create a class called `Animal` and a class called `Cat`.
 // `Cat` should extend the `Animal` class.
@@ -19,8 +27,58 @@
 // `meow` that should return the string `<name> meowed!` where `<name>` is the `name`
 // property set on the Cat instance.
 
+class Animal {
+  constructor(options) {
+    this.age = options.age;
+  }
+  growOlder() {
+    return (this.age += 1);
+  }
+}
+
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+    this.name = options.name;
+  }
+  meow() {
+    return `${this.name} meowed!`;
+  }
+}
+
+const jeff = new Cat({ age: 5, name: 'Jeff' });
+console.log(jeff.growOlder());
+console.log(jeff.meow());
+
+// class & extends practice
+
+class Human {
+  constructor(options) {
+    this.type = options.type;
+    this.firstName = options.firstName;
+    this.lastName = options.lastName;
+  }
+}
+
+class Person extends Human {
+  constructor(options) {
+    super(options);
+  }
+  greet() {
+    console.log(`My name is ${this.firstName} ${this.lastName}`);
+  }
+}
+
+const frank = new Person({
+  type: 'human',
+  firstName: 'Frank',
+  lastName: 'Faustino',
+});
+frank.greet();
+console.log(frank);
+console.log(frank.type);
 
 module.exports = {
   User,
-  Cat
+  Cat,
 };
