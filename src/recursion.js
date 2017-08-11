b6b97d755b58b96d59961732c7f9ee79244c92b4
Frank Faustino <nawzyah@gmail.com>
2017-08-11 01:25:59 -0700

    Completed most of recursion.js

--- src/recursion.js
+++ src/recursion.js
@@ -1,16 +1,18 @@
 // Complete the following functions.
 
-const nFibonacci = (n) => {
+const nFibonacci = n => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  return n < 2 ? 1 : nFibonacci(n - 2) + nFibonacci(n - 1);
 };
 
-const nFactorial = (n) => {
+const nFactorial = n => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  return n < 1 ? 1 : n * nFactorial(n - 1);
 };
 
-const checkMatchingLeaves = (obj) => {
+const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
 };
