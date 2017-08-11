aff0b73310bc17eead4b8563e53b74dbc1f11df0
Dylan <dylan@Dylans-MacBook.local>
2017-08-11 14:24:29 -0400

    optimized objects code

--- src/objects.js
+++ src/objects.js
@@ -18,21 +18,15 @@ const values = (obj) => {
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
-  const mapKeys = Object.keys(obj);
-  mapKeys.forEach((key) => {
-    obj[key] = cb(obj[key]);
-  });
+  keys(obj).forEach(k => obj[k] = cb(obj[k]));
   return obj;
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
-  const mapKeys = Object.keys(obj);
   const array = [];
-  mapKeys.forEach((key) => {
-    array.push([key, obj[key]]);
-  });
+  keys(obj).forEach(k => array.push([k, obj[k]]));
   return array;
 };
 
@@ -40,24 +34,16 @@ const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
-  const newObj = {};
-  const mapKeys = Object.keys(obj);
-  mapKeys.forEach((key) => {
-    newObj[obj[key]] = key;
-  });
-  return newObj;
+  const inv = {};
+  keys(obj).forEach(k => inv[obj[k]] = k);
+  return inv;
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
-  const mapKeys = Object.keys(defaultProps);
-  mapKeys.forEach((prop) => {
-    if (!obj[prop]) {
-      obj[prop] = defaultProps[prop];
-    }
-  });
+  Object.keys(defaultProps).forEach((d) => { if (!obj[d]) obj[d] = defaultProps[d]; });
   return obj;
 };
 
