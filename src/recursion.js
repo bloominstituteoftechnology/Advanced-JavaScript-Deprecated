fa1a3805c9148423540efaa93a6084495687a9ce
Kayce Hubbard <kayce.hubbard@gmail.com>
2017-08-11 19:26:50 -0500

    Update to pass all cases.

--- src/recursion.js
+++ src/recursion.js
@@ -13,11 +13,34 @@ const nFibonacci = (n) => {
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+
+  if (n === 0) {
+    return 1;
+  }
+  return nFactorial(n) * nFactorial(n - 1);
 };
 
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  let leafValue;
+  let leavesMatch = true;
+  const checkLeaf = (object) => {
+    Object.keys(object).forEach((key) => {
+      if (leafValue === undefined && key instanceof Object !== true) {
+        leafValue = object[key];
+        return undefined;
+      }
+      if (object[key] instanceof Object === true) return checkLeaf(object[key]);
+      if (object[key] !== leafValue) {
+        leavesMatch = false;
+        return undefined;
+      }
+      return undefined;
+    });
+  };
+  checkLeaf(obj);
+  return leavesMatch;
 };
 
 /* eslint-enable no-unused-vars */
