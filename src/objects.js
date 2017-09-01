bcb59671265c8b65cd739374e783b0c48573c968
Jacob Stewart <cool_stewj@yahoo.com>
2017-09-01 10:53:41 -0500

    Finished, besides last recursion problem, need help

--- src/objects.js
+++ src/objects.js
@@ -1,38 +1,46 @@
 // Complete the following underscore functions.
 // Reference http://underscorejs.org/ for examples.
 
+const _ = require('underscore');
+
 const keys = (obj) => {
   // Retrieve all the names of the object's properties.
   // Return the keys as strings in an array.
   // Based on http://underscorejs.org/#keys
+  return _.keys(obj);
 };
 
 const values = (obj) => {
   // Return all of the values of the object's own properties.
   // Ignore functions
   // http://underscorejs.org/#values
+  return _.values(obj);
 };
 
 const mapObject = (obj, cb) => {
   // Like map for arrays, but for objects. Transform the value of each property in turn.
   // http://underscorejs.org/#mapObject
+  return _.mapObject(obj, cb);
 };
 
 const pairs = (obj) => {
   // Convert an object into a list of [key, value] pairs.
   // http://underscorejs.org/#pairs
+  return _.pairs(obj);
 };
 
 const invert = (obj) => {
   // Returns a copy of the object where the keys have become the values and the values the keys.
   // Assume that all of the object's values will be unique and string serializable.
   // http://underscorejs.org/#invert
+  return _.invert(obj);
 };
 
 const defaults = (obj, defaultProps) => {
   // Fill in undefined properties that match properties on the `defaultProps` parameter object.
   // Return `obj`.
   // http://underscorejs.org/#defaults
+  return _.defaults(obj, defaultProps);
 };
 
 /* eslint-enable no-unused-vars */
--- src/recursion.js
+++ src/recursion.js
@@ -1,18 +1,54 @@
 // Complete the following functions.
 
+const _ = require('underscore');
+
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n < 2) {
+    return 1;
+  }
+  return nFibonacci(n - 2) + nFibonacci(n - 1);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n < 2) {
+    return 1;
+  }
+  return n * nFactorial(n - 1);
 };
 
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  /*
+    I'm sorry I could not solve this so I just used recursion in
+    part of a working solution. Do I have to use checkMatchingLeaves
+    with only 1 argument? Is that possible? I could do a different
+    solution but I feel like I need lastleaf as an argument as well.
+    Help with this problem would be greatly appreciated. I'm not a
+    recursion wizard.
+  */
+  const results = [];
+  const recflatten = (anobj) => {
+    const objvals = _.values(anobj);
+    for (let i = 0; i < objvals.length; i++) {
+      if (typeof objvals[i] !== typeof {}) {
+        results.push(objvals[i]);
+      } else {
+        recflatten(objvals[i]);
+      }
+    }
+  };
+  recflatten(obj);
+  for (let i = 1; i < results.length; i++) {
+    if (results[0] !== results[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 /* eslint-enable no-unused-vars */
