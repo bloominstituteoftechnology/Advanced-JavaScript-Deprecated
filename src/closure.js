d53dfa191d9d7a053725ae041d6c36c21185201f
Donnie Reese <donniereese@gmail.com>
2017-09-08 14:46:12 -0500

    Completed the closures and extra for that test.

--- src/closure.js
+++ src/closure.js
@@ -26,23 +26,34 @@ const counterFactory = () => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
-  let cbcount = n;
+
+  let cbcount = 0;
   const caller = cb;
 
-  return () => {
-    cbcount -= 1;
-    if (cbcount >= 0) caller();
+  return (...nums) => {
+    cbcount += 1;
+    if (cbcount <= n) {
+      return caller(...nums);
+    }
+    return null;
   };
 };
 
 /* Extra Credit */
 const cacheFunction = (cb) => {
-  // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
-  // If the returned function is invoked with arguments that it has already seen
-  // then it should return the cached result and not invoke `cb` again.
-  // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  // Should return a funciton that invokes `cb`.
+  return (arg) => {
+    // If the returned function is invoked with arguments that it has already seen
+    // then it should return the cached result and not invoke `cb` again.
+    // `cb` should only ever be invoked once for a given set of arguments.
+    if (!(arg in cache)) {
+      cache[arg] = cb(arg);
+    }
+    return cache[arg];
+  };
 };
 
 /* eslint-enable no-unused-vars */
