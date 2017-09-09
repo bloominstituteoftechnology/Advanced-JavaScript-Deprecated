b751be4c5ad0515b8875147fe7338ac9d42abb9c
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2017-09-09 11:54:10 -0400

    Project W1(B) - Completed
    -Arrays: Completed
    -Class: Completed
    -Closure: Completed
    -ES6: Completed
    -Objects: Completed
    -'This': Completed
    -Recursion: Completed

--- src/this.js
+++ src/this.js
@@ -11,18 +11,17 @@ class User {
   }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
-  // return `true` if they match, otherwise return `false` 
+  // return `true` if they match, otherwise return `false`
 }
 
 const me = new User({ username: 'LambdaSchool', password: 'correcthorsebatterystaple' });
 const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
 
-const checkPassword = (passwordToCompare) => this.password === passwordToCompare;
+const checkPassword = passwordToCompare => this.password === passwordToCompare;
   // recreate the `checkPassword` method that you made on the `User` class
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
-  // note that we use the `function` keyword and not `=>`
- 
+  // note that we use the `function` keyword and not `=>'
 
 // invoke `checkPassword` on `me` by explicitly setting the `this` context
 // use .call, .apply, and .bind
