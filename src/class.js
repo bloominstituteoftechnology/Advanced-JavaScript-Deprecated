e6e4fc1eb6ee300da3c72d4031f65a77a7435261
Tai N <ndhuutai1@gmail.com>
2017-09-11 10:48:17 -0700

    still working on the es6 and recursion, should have read the instructions better

--- src/class.js
+++ src/class.js
@@ -5,11 +5,22 @@
 // Add a method called `comparePasswords`.  `comparePasswords` should have a parameter
 // for a potential password that will be compared to the `password` property.
 // Return true if the potential password matches the `password` property.  Otherwise return false.
-
-
 /* eslint-disable no-undef */ // Remove this comment once you write your classes.
-
-
+// Create a class called User. // The constructor of the class should have a parameter called `options`.
+// `options` will be an object that will have the properties `email` and `password`.
+// Set the `email` and `password` properties on the class.
+// Add a method called `comparePasswords`.  `comparePasswords` should have a parameter
+// for a potential password that will be compared to the `password` property.
+// Return true if the potential password matches the `password` property.  Otherwise return false.
+class User {
+  constructor(options) {
+    this.email = options.email;
+    this.password = options.password;
+  }
+  comparePasswords(password) {
+    return this.password === password;
+  }
+}
 // Create a class called `Animal` and a class called `Cat`.
 // `Cat` should extend the `Animal` class.
 // Animal and Cat should both have a parameter called `options` in their constructors.
@@ -19,7 +30,26 @@
 // `meow` that should return the string `<name> meowed!` where `<name>` is the `name`
 // property set on the Cat instance.
 
+class Animal {
+  constructor(options) {
+    this.age = options.age;
+  }
 
+  growOlder(param) {
+    return ++this.age;
+  }
+}
+
+class Cat extends Animal {
+  constructor(options) {
+    super(options);
+    this.name = options.name;
+  }
+
+  meow() {
+    return `${this.name} meowed!`;
+  }
+}
 module.exports = {
   User,
   Cat
