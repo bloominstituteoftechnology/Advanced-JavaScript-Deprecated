4f3d764a4b756261944eac0f08479394011c91d4
Samer Dabra <samer.dabra@googlemail.com>
2017-09-12 20:04:41 +0100

    Completed

--- src/arrays.js
+++ src/arrays.js
@@ -8,33 +8,72 @@ const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   // based off http://underscorejs.org/#each
+  // for (let i = 0; i < elements.length; i++) {
+  //   cb(elements[i], i);
+  // }
+  elements.forEach((item, index) => {
+    cb(item, index);
+  });
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const arr = [];
+  each(elements, item => (arr.push(cb(item))));
+  return arr;
 };
 
 const reduce = (elements, cb, memo = elements.shift()) => {
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
-  // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+  // `memo` is the starting value.  elements[0]If `memo` is undefined then make `` the initial value.
+  each(elements, (item) => {
+    memo = cb(memo, item);
+  });
+  return memo;
 };
 
 const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) {
+      return elements[i];
+    }
+  }
+  return undefined;
+  // elements.forEach((item) => {
+  //   if (cb(item) === true) {
+  //     return item;
+  //   }
+  // });
+  // return undefined;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  const filtered = [];
+  each(elements, (item) => {
+    if (cb(item)) {
+      filtered.push(item);
+    }
+  });
+  return filtered;
 };
 
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  const flattened = reduce(elements, (memo, item) => {
+    if (Array.isArray(item)) {
+      return memo.concat(flatten(item));
+    }
+    return memo.concat(item);
+  }, []);
+  return flattened;
 };
 
 /* eslint-enable no-unused-vars, max-len */
--- src/recursion.js
+++ src/recursion.js
@@ -3,16 +3,38 @@
 const nFibonacci = (n) => {
   // fibonacci sequence: 1 2 3 5 8 13 ...
   // return the nth number in the sequence
+  if (n <= 1) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
 };
 
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n < 1) return 1;
+  return n * nFactorial(n - 1);
 };
 
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  let val;
+  let allMatch = true;
+  const checkLeaves = (object) => {
+    Object.keys(object).forEach((key) => {
+      if (val === undefined && typeof key !== 'object') {
+        val = object[key];
+        return undefined;
+      }
+      if (typeof object[key] === 'object') return checkLeaves(object[key]);
+      if (object[key] !== val) {
+        allMatch = false;
+        return undefined;
+      }
+      return undefined;
+    });
+  };
+  checkLeaves(obj);
+  return allMatch;
 };
 
 /* eslint-enable no-unused-vars */
