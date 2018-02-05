a0500c4110ad535441fe9cb7bb6db7a56b6466d7
Jeff <26851121+etisdew@users.noreply.github.com>
2018-02-05 00:48:01 -0800

    Update arrays.js

--- src/arrays.js
+++ src/arrays.js
@@ -5,35 +5,41 @@
 // You can use the functions that you have already written to help solve the other problems
 
 const each = (elements, cb) => {
+  // Reproduce the functionality of forEach from scratch.
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   // based off http://underscorejs.org/#each
 };
 
 const map = (elements, cb) => {
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
-  // Return the new array.
+  // Reproduce the functionality of map from scratch.
+  // Return a new array of values by mapping each value in a array through a transformation function (iteratee).
 };
 
 const reduce = (elements, cb, memo = elements.shift()) => {
+  // Reproduce the functionality of reduce from scratch.
   // Combine all elements into a single value going from left to right.
   // Elements will be passed one by one into `cb`.
   // `memo` is the starting value.  If `memo` is undefined then make `elements[0]` the initial value.
+  // Return the product of the callback.
 };
 
 const find = (elements, cb) => {
+  // Reproduce the functionality of find from scratch.
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
 };
 
 const filter = (elements, cb) => {
+  // Reproduce the functionality of filter from scratch.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
 };
 
 /* Extra Credit */
 const flatten = (elements) => {
+  // Reproduce the functionality of flatten from scratch.
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 };
